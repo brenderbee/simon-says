eeeb9410b3c44cbbd2fafa57df294550741dc5a3
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-04-14 12:59:33 -0700

    Refactored to DRY click events.

--- js/scripts.js
+++ js/scripts.js
@@ -5,27 +5,29 @@ function Player(counts) {
   this.counts = [];
 }
 
+function Quarter(value, sound, name) {
+  this.value = value;
+  this.sound = sound;
+  this.name = name;
+}
+
 var simon = new Player([]);
 var newPlayer = new Player([]);
 
-var sndLose = new Audio("sound/lose.wav")
-var sndTopRight = new Audio("sound/200.wav")
-var sndBottomRight = new Audio("sound/300.wav")
-var sndBottomLeft = new Audio("sound/400.wav")
-var sndTopLeft = new Audio("sound/500.wav")
-
-Player.prototype.getLength = function () {
-  this.counts.length;
-};
+var sndLose = new Audio("sound/lose.wav");
+var sndTopRight = new Audio("sound/200.wav");
+var sndBottomRight = new Audio("sound/300.wav");
+var sndBottomLeft = new Audio("sound/400.wav");
+var sndTopLeft = new Audio("sound/500.wav");
 
 Player.prototype.clearFields = function () {
   this.counts = [];
 };
 
-function random() {
-  var randomNumber = (Math.floor(Math.random()*4));
-  return randomNumber;
-}
+Quarter.prototype.playSound = function () {
+  this.sound.play();
+  setTimeout(function(){this.sound.pause()}, 500);
+};
 
 function light(number) {
   if (number === 0) {
@@ -58,6 +60,13 @@ function replay(array) {
   });
 }
 
+function random() {
+  var randomNumber = (Math.floor(Math.random()*4));
+  return randomNumber;
+}
+
+
+
 function resetGame() {
   counter = 0;
   click = 0;
@@ -72,6 +81,20 @@ function simonTurn() {
   counter += 1
   $(".counter p").text(counter);
   replay(simon.counts);
+}
+
+function playerTurn(sound, number) {
+  sound.play();
+  setTimeout(function(){sound.pause()}, 500);
+  if (simon.counts[click] === number && click < simon.counts.length - 1) {
+    click += 1;
+  } else if (simon.counts[click] === number && click === simon.counts.length - 1) {
+    simonTurn();
+  } else {
+    sndLose.play();
+    setTimeout(function(){sndLose.pause()}, 300);
+    return resetGame();
+  }
 }
 
 
@@ -89,58 +112,18 @@ $(document).ready(function() {
   });
 
   $(".top-right").click(function(){
-    sndTopRight.play();
-    setTimeout(function(){sndTopRight.pause()}, 500);
-    if (simon.counts[click] === 0 && click < simon.counts.length - 1) {
-      click += 1;
-    } else if (simon.counts[click] === 0 && click === simon.counts.length - 1) {
-      simonTurn();
-    } else {
-      sndLose.play();
-      setTimeout(function(){sndLose.pause()}, 300);
-      return resetGame();
-    }
+    playerTurn(sndTopRight, 0);
   });
 
   $(".bottom-right").click(function(){
-    sndBottomRight.play();
-    setTimeout(function(){sndBottomRight.pause()}, 500);
-    if (simon.counts[click] === 1 && click < simon.counts.length - 1) {
-      click += 1;
-    } else if (simon.counts[click] === 1 && click === simon.counts.length - 1) {
-      simonTurn();
-    } else {
-      sndLose.play();
-      setTimeout(function(){sndLose.pause()}, 300);
-      return resetGame();
-    }
+    playerTurn(sndBottomRight, 1);
   });
 
   $(".bottom-left").click(function(){
-    sndBottomLeft.play();
-    setTimeout(function(){sndBottomLeft.pause()}, 500);
-    if (simon.counts[click] === 2 && click < simon.counts.length - 1) {
-      click += 1;
-    } else if (simon.counts[click] === 2 && click === simon.counts.length - 1) {
-      simonTurn();
-    } else {
-      sndLose.play();
-      setTimeout(function(){sndLose.pause()}, 300);
-      return resetGame();
-    }
+    playerTurn(sndBottomLeft, 2);
   });
 
   $(".top-left").click(function(){
-    sndTopLeft.play();
-    setTimeout(function(){sndTopLeft.pause()}, 500);
-    if (simon.counts[click] === 3 && click < simon.counts.length - 1) {
-      click += 1;
-    } else if (simon.counts[click] === 3 && click === simon.counts.length - 1) {
-      simonTurn();
-    } else {
-      sndLose.play();
-      setTimeout(function(){sndLose.pause()}, 300);
-      return resetGame();
-    }
+    playerTurn(sndTopLeft, 3);
   });
 });
